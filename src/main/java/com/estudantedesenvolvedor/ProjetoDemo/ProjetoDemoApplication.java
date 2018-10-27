2a85f0cbf409501b38af22b418b889a945c770fc
AnotherAlexMoreira <alexmartinhomoreira@gmail.com>
2018-10-27 15:48:46 -0300

    Projeto Inicial

---
+++ src/main/java/com/estudantedesenvolvedor/ProjetoDemo/ProjetoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.estudantedesenvolvedor.ProjetoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDemoApplication.class, args);
+		System.out.println("Teste");
+	}
+}
